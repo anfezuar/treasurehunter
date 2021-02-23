8ced5ddeb5ab91a84018ab3361454e3f0e3422f2
Andres Felipe Zuluaga Arias <anfezuar270827@gmail.com>
2021-02-23 18:06:04 -0500

    Create screen Home

---
+++ src/app/screens/Home/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { TouchableOpacity, View, Text } from 'react-native';
+
+import styles from './styles';
+
+function Home() {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.cardGame}>
+        <Text style={styles.textCard}>INICIAR JUEGO</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.cardGame}>
+        <Text style={styles.textCard}>CREAR JUEGO</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+export default Home;
